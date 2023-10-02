1390a76eb2eb100d4762d3ba4b129afcb9547376
YES-IKO <yesikojnsinaga@gmail.com>
2023-10-02 22:53:16 +0700

    first commit

---
+++ JsFundamentals/variables/index.js
@@ -0,0 +1,51 @@
+// Real Life Analagy
+
+// imagine it as a "box" for Data, with uniquely-named sticker on it.
+
+// for instance the variable "message" can be imagined as a box labeled "message" with value "Hello" in it:
+
+// we can put any value in the box.
+
+// we can change it as many time as we want (let)
+
+let message;
+message = "Hello";
+console.log(message);
+
+let $usd = 12_000;
+console.log($usd);
+
+let _aqua = 5000;
+console.log(_aqua);
+
+let soccerPlayer = 11;
+soccerPlayer += 5;
+console.log(soccerPlayer);
+
+let platoonPersonel = 30;
+platoonPersonel /= 3;
+console.log(platoonPersonel);
+
+// Task
+let name = "John";
+let admin = name;
+console.log(admin);
+
+// Task2
+let ourPlanetName = "Earth";
+let currentVisitor = "John";
+console.log((currentVisitor += " visit " + ourPlanetName)); // concatenation operator +
+
+// Trial only
+// const birthday = "30.04.2004";
+// const age = somecode(birtday);
+
+let firstName = "John";
+let lastName = "Doe";
+let age = 25;
+let isMarried = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
